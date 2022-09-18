4fb140e6177ebb18b98057fe7a2e0b565f07f90e
unknown <charles.chrismann@free.fr>
2022-09-18 15:01:28 +0200

    :twisted_rightwards_arrows: Merge branch charles

--- charles/main.js
+++ charles/main.js
@@ -8,12 +8,5 @@ function getScrollPercent() {
 }
 
 document.querySelector("main").addEventListener("scroll", (e) => {
-    console.log(getScrollPercent())
     document.querySelector(".scene").style.transform = `rotateY(${getScrollPercent() / 100 * 240}deg)`
-    // document.querySelector(".scene").style.transform = `rotateY(${getScrollPercent() / 100 * 240}deg) translateY(${getScrollPercent() / 100 * 900}px)`
-    // console.log(`-${getScrollPercent() * 2}px`)
-    // document.querySelector(".scene").style.top = `-${getScrollPercent() / 100 * 300}px`
-
-    // n2 : top -80px 
-    // n3 : top 160px 
 })
